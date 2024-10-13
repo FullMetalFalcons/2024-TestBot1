c0d60b9583f28404da08f696ac9dc4aa6dfd216c
Stephen Tine <stephen@tinefamily.com>
2024-10-12 22:21:57 -0400

    Arm moves during SquareTest TeleOpTest added and working with drive/arm controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SquareTestRoute.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SquareTestRoute.java
@@ -21,11 +21,81 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+/*
+  Wireless Code Download: Terminal --> "adb connect 192.168.43.1:5555"
+ */
+
+
 @Config
 @Autonomous
 public class SquareTestRoute extends LinearOpMode {
+
+    public class Arm {
+        private DcMotorEx arm;
+
+        public Arm(HardwareMap hardwareMap) {
+            arm = hardwareMap.get(DcMotorEx.class, "arm");
+            arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            arm.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            // Reset the arm's position, then let the code control it
+            arm.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+            arm.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        }
+
+        public class ArmOffFloor implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    arm.setPower(0.8);
+                    initialized = true;
+                }
+
+                double position = arm.getCurrentPosition();
+                packet.put("Arm Position", position);
+                if (position < 500.0) {
+                    return true;
+                } else {
+                    arm.setPower(0);
+                    return false;
+                }
+            }
+        }
+        public Action ArmOffFloor() {
+            return new ArmOffFloor();
+        }
+
+        public class ArmToFloor implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    arm.setPower(-0.8);
+                    initialized = true;
+                }
+
+                double position = arm.getCurrentPosition();
+                packet.put("Arm Position", position);
+                if (position > 0.0) {
+                    return true;
+                } else {
+                    arm.setPower(0);
+                    return false;
+                }
+            }
+        }
+        public Action ArmToFloor(){
+            return new ArmToFloor();
+        }
+    }
+
+
     public void runOpMode() {
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, Math.toRadians(90)));
+        Arm arm = new Arm(hardwareMap);
 
         Action trajectoryAction1;
         Action trajectoryAction2;
@@ -67,7 +137,9 @@ public class SquareTestRoute extends LinearOpMode {
 
         Actions.runBlocking(
                 new SequentialAction(
-                        trajectoryActionChosen
+                        arm.ArmOffFloor(),
+                        trajectoryActionChosen,
+                        arm.ArmToFloor()
                 )
         );
     }
